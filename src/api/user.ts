d919aea506caafa109b3b4d0644ec66209f388fc
Joshhsieh <halu_9@hotmail.com>
2025-12-19 15:14:02 +0800

    first commit

--- src/api/user.ts
+++ src/api/user.ts
@@ -3,6 +3,9 @@ import axios, { type AxiosResponse } from 'axios'
 
 const BASE_URL = import.meta.env.VITE_BASE_URL
 
+console.log('BASE_URL', BASE_URL);
+
+
 const userApi = axios.create({
   baseURL: BASE_URL,
 })
